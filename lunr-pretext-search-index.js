3574b97401dd81cc69533834577456f97c02cb56
camiloaugustov28-cmyk <camiloaugustov28@gmail.com>
2026-03-06 23:37:06 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -15,8 +15,8 @@ var ptx_lunr_docs = [
   "url": "sec-section-PointsOnGraphs.html",
   "type": "Section",
   "number": "1.1",
-  "title": "Section Title",
-  "body": " Section Title  While the corresponding points on the graphs of vertical transformations may seem clear, the corresponding points on the graphs of the horizontal transformations can create some confusion. Let's see the logic argument behind the characterization we observed.     Let the graph of be a horizontal shift of units of the graph of . If the point lies on the graph of , then the point lies on the graph of .     "
+  "title": "Effect of transformations on points",
+  "body": " Effect of transformations on points  While the corresponding points on the graphs of vertical transformations may seem clear, the corresponding points on the graphs of the horizontal transformations can create some confusion. Let's see the logic argument behind the characterization we observed.     Let the graph of be a horizontal shift of units of the graph of . If the point lies on the graph of , then the point lies on the graph of .       Let be a function with domain . By definition Let be a function with domain defined by (a horizontal shift of by units.) Then . Write for some . Then     "
 },
 {
   "id": "prop-PointsHorizontalShift",
@@ -26,6 +26,15 @@ var ptx_lunr_docs = [
   "number": "1.1.1",
   "title": "",
   "body": "  Let the graph of be a horizontal shift of units of the graph of . If the point lies on the graph of , then the point lies on the graph of .   "
+},
+{
+  "id": "sec-section-PointsOnGraphs-4-1",
+  "level": "2",
+  "url": "sec-section-PointsOnGraphs.html#sec-section-PointsOnGraphs-4-1",
+  "type": "Proof",
+  "number": "1",
+  "title": "",
+  "body": " Let be a function with domain . By definition Let be a function with domain defined by (a horizontal shift of by units.) Then . Write for some . Then   "
 },
 {
   "id": "backmatter-2",
