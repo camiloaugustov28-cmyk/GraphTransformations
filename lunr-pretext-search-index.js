12230b0a19cffee57076613db23587a319dc298b
camiloaugustov28-cmyk <camiloaugustov28@gmail.com>
2026-04-13 22:35:03 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,11 +10,56 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
+  "id": "sec-section-OrderVertical",
+  "level": "1",
+  "url": "sec-section-OrderVertical.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Order on vertical transformations",
+  "body": " Order on vertical transformations  We will see how to apply more than one vertical transformation on the same graph.  Since vertical dilations, reflections across the axis, and vertical shifts act all on values, it is natural to wonder if applying them in any order will produce the same final graph. Let's see how these transformations interact using the below graph.  We will start with the graph of a function and will consider the transformation . We will work using a two-step procedure as follows     Set and .    Define and . The final graph will be the graph of shown in blue.    Now, define and . The final graph will be the graph of show in purple.        Order in vertical transformations    As you can see, the graphs obtained for step 2 and step 3 are different. Let's discuss what's happening there.    Multiple-Choice, Randomized, One Answer  vert shift first   To obtain the graph of , we      apply a vertical shift 3 down first, followed by a vertical stretch by a factor of 2      apply a vertical shift 3 up first, followed by a vertical stretch by a factor of 2      apply a vertical stretch by a factor of 2 first, followed by a vertical shift 3 units up      apply a vertical compression by a factor of 2 first, followed by a vertical shift 3 units up       Multiple-Choice, Randomized, One Answer  vert shift first   To obtain the graph of , we      apply a vertical shift 3 down first, followed by a vertical stretch by a factor of 2      apply a vertical shift 3 up first, followed by a vertical stretch by a factor of 2      apply a vertical stretch by a factor of 2 first, followed by a vertical shift 3 units up      apply a vertical compression by a factor of 2 first, followed by a vertical shift 3 units up       It remains to see whether the graph of or the graph of corresponds to the correct graph of . To see this, we define in the applet above, and we click on the circle on the left to make its graph visible (in green). The graph will agree with either the graph of or the graph of .    Multiple-Choice, Randomized, One Answer  vert shift first   To obtain the graph of , we      apply a vertical shift 3 down first, followed by a vertical stretch by a factor of 2      apply a vertical shift 3 up first, followed by a vertical stretch by a factor of 2      apply a vertical stretch by a factor of 2 first, followed by a vertical shift 3 units up      apply a vertical compression by a factor of 2 first, followed by a vertical shift 3 units up       "
+},
+{
+  "id": "sec-section-OrderVertical-6-1",
+  "level": "2",
+  "url": "sec-section-OrderVertical.html#sec-section-OrderVertical-6-1",
+  "type": "Figure",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  Order in vertical transformations  "
+},
+{
+  "id": "multiple-choice-Orderq",
+  "level": "2",
+  "url": "sec-section-OrderVertical.html#multiple-choice-Orderq",
+  "type": "Exercise",
+  "number": "1.1.1",
+  "title": "Multiple-Choice, Randomized, One Answer.",
+  "body": " Multiple-Choice, Randomized, One Answer  vert shift first   To obtain the graph of , we      apply a vertical shift 3 down first, followed by a vertical stretch by a factor of 2      apply a vertical shift 3 up first, followed by a vertical stretch by a factor of 2      apply a vertical stretch by a factor of 2 first, followed by a vertical shift 3 units up      apply a vertical compression by a factor of 2 first, followed by a vertical shift 3 units up     "
+},
+{
+  "id": "multiple-choice-Orderh",
+  "level": "2",
+  "url": "sec-section-OrderVertical.html#multiple-choice-Orderh",
+  "type": "Exercise",
+  "number": "1.1.2",
+  "title": "Multiple-Choice, Randomized, One Answer.",
+  "body": " Multiple-Choice, Randomized, One Answer  vert shift first   To obtain the graph of , we      apply a vertical shift 3 down first, followed by a vertical stretch by a factor of 2      apply a vertical shift 3 up first, followed by a vertical stretch by a factor of 2      apply a vertical stretch by a factor of 2 first, followed by a vertical shift 3 units up      apply a vertical compression by a factor of 2 first, followed by a vertical shift 3 units up     "
+},
+{
+  "id": "multiple-choice-OrderA2k3",
+  "level": "2",
+  "url": "sec-section-OrderVertical.html#multiple-choice-OrderA2k3",
+  "type": "Exercise",
+  "number": "1.1.1",
+  "title": "Multiple-Choice, Randomized, One Answer.",
+  "body": " Multiple-Choice, Randomized, One Answer  vert shift first   To obtain the graph of , we      apply a vertical shift 3 down first, followed by a vertical stretch by a factor of 2      apply a vertical shift 3 up first, followed by a vertical stretch by a factor of 2      apply a vertical stretch by a factor of 2 first, followed by a vertical shift 3 units up      apply a vertical compression by a factor of 2 first, followed by a vertical shift 3 units up     "
+},
+{
   "id": "sec-section-PointsOnGraphs",
   "level": "1",
   "url": "sec-section-PointsOnGraphs.html",
   "type": "Section",
-  "number": "1.1",
+  "number": "2.1",
   "title": "Effect of transformations on points",
   "body": " Effect of transformations on points  While the corresponding points on the graphs of vertical transformations may seem clear, the corresponding points on the graphs of the horizontal transformations can create some confusion. Let's see the logic argument behind the characterization we observed.     Let the graph of be a horizontal shift of units of the graph of . If the point lies on the graph of , then the point lies on the graph of .       Let be a function with domain . By definition Let be a function with domain defined by (a horizontal shift of by units.) Then . Write for some . Then        Let the graph of be a horizontal dilation of the graph of , i.e., . If the point lies on the graph of , then the point lies on the graph of .       Let be a function with domain . By definition Let be a function with domain defined by (a horizontal dilation of .) Then . Write for some . Then     "
 },
@@ -23,7 +68,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-PointsOnGraphs.html#prop-PointsHorizontalShift",
   "type": "Proposition",
-  "number": "1.1.1",
+  "number": "2.1.1",
   "title": "",
   "body": "  Let the graph of be a horizontal shift of units of the graph of . If the point lies on the graph of , then the point lies on the graph of .   "
 },
@@ -41,7 +86,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-PointsOnGraphs.html#prop-PointsHorizontalDilation",
   "type": "Proposition",
-  "number": "1.1.2",
+  "number": "2.1.2",
   "title": "",
   "body": "  Let the graph of be a horizontal dilation of the graph of , i.e., . If the point lies on the graph of , then the point lies on the graph of .   "
 },
@@ -59,7 +104,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-section-TransformationsARC.html",
   "type": "Section",
-  "number": "1.2",
+  "number": "2.2",
   "title": "Effect of transformations on the average rate of change",
   "body": " Effect of transformations on the average rate of change  Now we formalize the observed relations between the average rate of change of a function and the average rate of change of each of its respective transformations.     If , then        Since is a vertical transformation of , the values are not affected by the transformation. So, we compute the average rate of change of over the same interval we compute the average rate of change of . We have        If , then        Since is a vertical transformation of , the values are not affected by the transformation. So, we compute the average rate of change of over the same interval we compute the average rate of change of . We have        If , then        Since is a horizontal shift of , if and are values for the graph of , then and are the corresponding values on the graph of . So, if the average rate of change of is computed on the interval , then the corresponding average rate of change of needs to be computed on the interval . We have        If , then        Since is a horizontal dilation of , if and are values for the graph of , then and are the corresponding values on the graph of . So, if the average rate of change of is computed on the interval , then the corresponding average rate of change of needs to be computed on the interval . We have     "
 },
@@ -68,7 +113,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-TransformationsARC.html#prop-ARCVerticalShift",
   "type": "Proposition",
-  "number": "1.2.1",
+  "number": "2.2.1",
   "title": "",
   "body": "  If , then    "
 },
@@ -86,7 +131,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-TransformationsARC.html#prop-ARCVerticalDilation",
   "type": "Proposition",
-  "number": "1.2.2",
+  "number": "2.2.2",
   "title": "",
   "body": "  If , then    "
 },
@@ -104,7 +149,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-TransformationsARC.html#prop-ARCHorizontalShift",
   "type": "Proposition",
-  "number": "1.2.3",
+  "number": "2.2.3",
   "title": "",
   "body": "  If , then    "
 },
@@ -122,7 +167,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-TransformationsARC.html#prop-ARCHorizontalDilation",
   "type": "Proposition",
-  "number": "1.2.4",
+  "number": "2.2.4",
   "title": "",
   "body": "  If , then    "
 },
